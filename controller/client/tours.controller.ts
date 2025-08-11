ca602b99ed38e7b96811154544d3bb9c8a05fb8b
Nguyễn Văn Sâm <vansam09082004@gmail.com>
2025-08-11 20:21:30 +0700

    cau hinh controller

---
+++ controller/client/tours.controller.ts
@@ -0,0 +1,15 @@
+
+import { Request, Response } from "express";
+import Tour from "../../model/tours.model"
+export const getAllTours = async (req: Request, res: Response): Promise<void> => {
+  const tours = await Tour.findAll({
+    where:{
+        deleted: false ,
+        status:"active"
+    },
+    raw: true,
+  });
+
+  res.render("client/pages/tours/index.pug", { tours });
+};
+
--- router/client/tours.router.ts
+++ router/client/tours.router.ts
@@ -1,13 +1,7 @@
 import express, {Request , Response } from "express";
 import Tour from "../../model/tours.model"
 const router = express.Router();
-
-router.get("/", async (req: Request, res: Response) => {
-  const tours = await Tour.findAll({
-    raw: true,
-  });
-  console.log(tours);
-  res.render("client/pages/tours/index.pug", { tours });
-});
+import { getAllTours } from "../../controller/client/tours.controller";
+router.get("/", getAllTours);
 
 export default router;
