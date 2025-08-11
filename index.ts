30e47aa0c07ff3196676cec309daabe7c66559df
Nguyễn Văn Sâm <vansam09082004@gmail.com>
2025-08-11 20:38:46 +0700

    nhúng file tĩnh

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ sequelize.authenticate()
   .catch((err) => {
     console.error("Unable to connect to the database:", err);
   });
-
+app.use(express.static(path.join(__dirname, 'public')));
 // 7. View engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
