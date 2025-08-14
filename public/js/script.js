553d99a3694655df2cb37e6ab4d83ae4e8003439
Nguyễn Văn Sâm <vansam09082004@gmail.com>
2025-08-14 15:53:24 +0700

    cập nhập số sản phẩm trên giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,42 @@
         swiper: swiper,
       },
     });
+const showminiCart = () => {
+
+  
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const totalQuantity = cart.reduce((total, item) => total + item.quantity, 0);
+    console.log(totalQuantity);
+    const minicart = document.querySelector("[mini-cart-quantity]");
+    minicart.innerHTML = totalQuantity;
+
+
+};
+
+showminiCart();
+
+    // thêm tua vào giỏ hàng 
+
+    const formAddCart = document.querySelector('form.form-add-to-cart');
+    formAddCart.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+     const quantity=parseInt(e.target.elements.quantity.value);
+     const tourId=parseInt(formAddCart.getAttribute('tour-id'));
+     if(quantity>0 && tourId){
+      const cart=JSON.parse(localStorage.getItem('cart')) || [];
+      const existingItem = cart.find(item => item.tourId === tourId);
+      if (existingItem) {
+        existingItem.quantity += quantity;
+      } else {
+        cart.push({ tourId, quantity });
+      }
+      localStorage.setItem('cart', JSON.stringify(cart));
+      
+      alert('Thêm tour vào giỏ hàng thành công!');
+      showminiCart();
+     }
+
+
+    });
+
