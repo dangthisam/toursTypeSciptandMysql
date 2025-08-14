027a4020b3e03b7b9372903791971f8c8c494774
Nguyễn Văn Sâm <vansam09082004@gmail.com>
2025-08-14 13:49:23 +0700

    slide avatar tuours

--- controller/client/tours.controller.ts
+++ controller/client/tours.controller.ts
@@ -40,3 +40,42 @@ console.log(tours)
     res.status(500).send("Lỗi server, vui lòng thử lại sau.");
   }
 };
+
+export const detailTours = async (req: Request, res: Response) => {
+  const { slugTour } = req.params;
+
+  try {
+    const tour = await sequelize.query(
+      `
+        SELECT tours.*, round(price * (1 - discount / 100), 0) AS price_special
+        FROM tours
+        WHERE tours.slug = :slugTour
+          AND tours.deleted = false
+          AND tours.status = 'active'
+      `,
+      {
+        replacements: { slugTour },
+        type: QueryTypes.SELECT,
+      }
+    );
+
+    if (tour.length === 0) {
+      return res.status(404).send("Tour not found");
+    }
+
+    const tourDetail = tour[0];
+    if (tourDetail["images"]) {
+      const images = JSON.parse(tourDetail["images"]);
+      tourDetail["image"] = images;
+    }
+    tourDetail["price_special"] = parseFloat(tourDetail["price_special"]);
+console.log(tourDetail)
+    res.render("client/pages/tours/detail.pug", {
+      tour: tourDetail,
+      pageTitle: "Chi tiết tour",
+    });
+  } catch (error) {
+    console.error("Error fetching tour details:", error);
+    res.status(500).send("Lỗi server, vui lòng thử lại sau.");
+  }
+};
--- router/client/tours.router.ts
+++ router/client/tours.router.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const router = express.Router();
-import { getAllTours } from "../../controller/client/tours.controller";
+import { getAllTours , detailTours } from "../../controller/client/tours.controller";
 router.get("/:slugCategory", getAllTours);
-
+router.get("/detail/:slugTour" , detailTours);
 export default router;
